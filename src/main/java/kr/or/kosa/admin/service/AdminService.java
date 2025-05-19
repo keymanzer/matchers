e2275d7dd114bfefdc335a91f6364dd8e36fcc86
minsik99 <kimminsik0726@gmail.com>
2025-05-19 16:49:12 +0900

    feat: 관리자 (전문가 등록 인증) 기능 추가

---
+++ src/main/java/kr/or/kosa/admin/service/AdminService.java
@@ -0,0 +1,21 @@
+package kr.or.kosa.admin.service;
+
+import java.util.List;
+
+import kr.or.kosa.admin.dto.ExpertCategory;
+import kr.or.kosa.admin.dto.ExpertDetail;
+import kr.or.kosa.admin.dto.ExpertLicense;
+import kr.or.kosa.admin.dto.ExpertLocation;
+
+public interface AdminService {
+
+	List<ExpertDetail> getExpertList();
+
+	List<ExpertLicense> getLicenses(Long userId);
+
+	List<ExpertCategory> getCategories(Long userId);
+
+	List<ExpertLocation> getLocations(Long userId);
+
+	void approveExpertByEmail(String email);
+}
---
+++ src/main/java/kr/or/kosa/mapper/AdminMapper.java
@@ -0,0 +1,26 @@
+package kr.or.kosa.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.or.kosa.admin.dto.ExpertCategory;
+import kr.or.kosa.admin.dto.ExpertDetail;
+import kr.or.kosa.admin.dto.ExpertLicense;
+import kr.or.kosa.admin.dto.ExpertLocation;
+
+@Mapper
+public interface AdminMapper {
+
+	List<ExpertDetail> getExpertList();
+
+	List<ExpertLicense> getLicenses(Long userId);
+
+	List<ExpertCategory> getCategories(Long userId);
+
+	List<ExpertLocation> getLocations(Long userId);
+
+	void approveExpertByEmail(String email);
+
+	void insertExpertAuth(String email);
+}
